6d9aa570db7448d44d16fc2fd5cc8923d1757504
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-05-08 11:18:37 -0500

    First commit

---
+++ Gramatical-Tree/src/model/NTreeNode.java
@@ -0,0 +1,34 @@
+package model;
+
+public class NTreeNode <T> {
+    protected T info;
+    protected SimpleList <NTreeNode<T>> childs;
+
+    public NTreeNode(T info) {
+        this.info = info;
+        this.childs = new SimpleList<>();
+    }
+    
+    public NTreeNode(T info, SimpleList<NTreeNode<T>> childs) {
+        this.info = info;
+        this.childs = childs;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public SimpleList<NTreeNode<T>> getChilds() {
+        return childs;
+    }
+
+    public void setChilds(SimpleList<NTreeNode<T>> childs) {
+        this.childs = childs;
+    }
+    
+    
+}
---
+++ Gramatical-Tree/src/model/SimpleNode.java
@@ -0,0 +1,32 @@
+package model;
+
+public class SimpleNode <T> {
+  protected T info;
+  protected SimpleNode <T> next;
+
+  public SimpleNode(T info, SimpleNode<T> next) {
+    this.info = info;
+    this.next = next;
+  }
+  
+  public SimpleNode(T info) {
+    this.info = info;
+    this.next = null;
+  }
+
+  public T getInfo() {
+    return info;
+  }
+
+  public void setInfo(T info) {
+    this.info = info;
+  }
+
+  public SimpleNode<T> getNext() {
+    return next;
+  }
+
+  public void setNext(SimpleNode<T> next) {
+    this.next = next;
+  }
+}
